ab3553227dd24be0348e9e961326f26da7b7d18c
KimJi-An <j0012a@naver.com>
2024-11-03 00:58:56 +0900

    feat: 구인글 마감 API 연결

--- src/features/recruitments/RecruitmentList/RecruitmentsTable/RecruitmentsTable.tsx
+++ src/features/recruitments/RecruitmentList/RecruitmentsTable/RecruitmentsTable.tsx
@@ -3,6 +3,8 @@ import { RecruitmentItem } from '@/types';
 import { buttonGroupStyle, buttonStyle, recruitmentStyle, recruitmentTitleStyle } from './RecruitmentsTable.styles';
 import { useNavigate, useParams } from 'react-router-dom';
 import ROUTE_PATH from '@/routes/path';
+import { useCloseRecruitment } from '@/apis/recruitments/hooks/useCloseRecruitment';
+import { useState } from 'react';
 
 type Props = {
   recruitmentList: RecruitmentItem[];
@@ -11,11 +13,28 @@ type Props = {
 export default function RecruitmentsTable({ recruitmentList }: Props) {
   const navigate = useNavigate();
   const { companyId } = useParams();
+  const mutation = useCloseRecruitment();
+  const [closedRecruitment, setClosedRecruitment] = useState<{ [key: number]: boolean }>({});
 
   const handleApplicantClick = (companyId: string, recruitmentId: number) => {
     navigate(
       ROUTE_PATH.APPLICANTS.replace(':companyId', companyId).replace(':recruitmentId', recruitmentId.toString()),
     );
+  };
+
+  const handleCloseRecruitmentClick = (recruitmentId: number) => {
+    mutation.mutate(recruitmentId, {
+      onSuccess: () => {
+        setClosedRecruitment((prev) => ({
+          ...prev,
+          [recruitmentId]: true,
+        }));
+      },
+      onError: () => {
+        // TODO: 에러 처리 결정
+        alert('마감 실패');
+      },
+    });
   };
 
   return (
@@ -49,7 +68,14 @@ export default function RecruitmentsTable({ recruitmentList }: Props) {
                     >
                       지원자 보러가기
                     </Button>
-                    <Button css={buttonStyle}>마감하기</Button>
+                    <Button
+                      css={buttonStyle}
+                      onClick={() => handleCloseRecruitmentClick(recruitment.recruitmentId)}
+                      design={closedRecruitment[recruitment.recruitmentId] ? 'deactivate' : 'default'}
+                      disabled={closedRecruitment[recruitment.recruitmentId]}
+                    >
+                      {closedRecruitment[recruitment.recruitmentId] ? '마감완료' : '마감하기'}
+                    </Button>
                   </Flex>
                 </Flex>
               </Td>
