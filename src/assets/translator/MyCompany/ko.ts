054d7173d5e69f89dfbeaa990103b321dcddc661
KimJi-An <j0012a@naver.com>
2024-11-04 21:06:09 +0900

    feat: 번역 데이터 작성

---
+++ src/assets/translator/MyCompany/ko.ts
@@ -0,0 +1,13 @@
+export const MyCompany = {
+  MY_RECRUITMENTS: '내 공고글',
+  TOTAL_RECRUITMENTS: (count: number) => `총 ${count}건`,
+  TABLE_HEADERS: {
+    LOCATION: '근무지',
+    TITLE: '공고 제목',
+  },
+  BUTTONS: {
+    VIEW_APPLICANTS: '지원자 보러가기',
+    CLOSE_RECRUITMENT: '마감하기',
+    CLOSED_RECRUITMENT: '마감완료',
+  },
+};
