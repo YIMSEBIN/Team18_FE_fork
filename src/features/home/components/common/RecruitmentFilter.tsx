2417f5d176b73de2fb9c9a5007ceb78568f467ed
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-11-08 13:04:07 +0900

    feat: add useRecruitmentData hooks

--- src/features/home/components/common/RecruitmentFilter.tsx
+++ src/features/home/components/common/RecruitmentFilter.tsx
@@ -6,29 +6,36 @@ import { useTranslation } from 'react-i18next';
 
 const triggerStyle = { minWidth: '80px', fontSize: '16px', fontWeight: '700', color: theme.palettes.blue };
 
-export default function RecruitmentFilter() {
+type Props = {
+  onFilterChange: (option: string) => void;
+};
+
+export default function RecruitmentFilter({ onFilterChange }: Props) {
   const { t } = useTranslation();
+
   const filterOptions = useMemo(
     () => [
       {
         value: 'all',
         text: t('home.recruitmentFilter.all'),
-        action: () => console.log('All clicked'),
       },
       {
         value: 'salary',
         text: t('home.recruitmentFilter.salary'),
-        action: () => console.log('Salary clicked'),
       },
       {
         value: 'latestRegistration',
         text: t('home.recruitmentFilter.latestRegistration'),
-        action: () => console.log('LatestRegistration clicked'),
       },
     ],
     [t],
   );
   const { selectedOption, handleSelect } = useSelect(filterOptions[0]);
+
+  const handleOptionSelect = (option: { value: string; text: string }) => {
+    handleSelect(option);
+    onFilterChange(option.value);
+  };
 
   return (
     <Select.Root>
@@ -39,7 +46,7 @@ export default function RecruitmentFilter() {
         <List
           items={filterOptions}
           renderItem={(option) => (
-            <Select.Option key={option.value} value={option.value} onClick={() => handleSelect(option)}>
+            <Select.Option key={option.value} value={option.value} onClick={() => handleOptionSelect(option)}>
               {option.text}
             </Select.Option>
           )}
